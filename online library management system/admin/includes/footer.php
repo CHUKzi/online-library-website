b6384046d9fa359880c2fcad2190b885cad4f0db
Royan Harsha <64635379+CHUKzi@users.noreply.github.com>
2022-04-23 21:03:09 +0530

    Delete online library management system directory

--- online library management system/admin/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="container-fluid">
-  <p>&copy; <?php echo date("Y"); ?> Developed By <a style="color: #FFFFFF;" href="https://alexlanka.com/" target="_blank">alexlanka.com</a></p>
-</footer>
--- online library management system/includes/query.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$query = "SELECT * FROM settings";
-	$settings = mysqli_query($connection, $query);
-	$setting = mysqli_fetch_assoc($settings);
-
-	if ($setting["stats"]==1) {
-		header('Location: under-construction.php');
-	}
-?>
